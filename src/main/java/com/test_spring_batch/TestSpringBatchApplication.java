e554bdcd9b2e5625eb76313744088e90f779d91a
Julio Ramirez <rrodriguez.julio@gmail.com>
2024-09-03 18:51:08 -0600

    starting new project test_spring_batch

---
+++ src/main/java/com/test_spring_batch/TestSpringBatchApplication.java
@@ -0,0 +1,13 @@
+package com.test_spring_batch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestSpringBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestSpringBatchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/test_spring_batch/TestSpringBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test_spring_batch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestSpringBatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
