4b0563bb41203cf478a3f5ec4be823b643655a67
Julio Ramirez <rrodriguez.julio@gmail.com>
2024-09-04 13:21:41 -0600

    add cron Job with a cron like '0 * 19 * * ?'

--- src/main/java/com/test_spring_batch/service/Scheduler.java
+++ src/main/java/com/test_spring_batch/service/Scheduler.java
@@ -1,7 +1,15 @@
 package com.test_spring_batch.service;
 
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Scheduler {
+
+  @Scheduled(cron = "0 * 19 * * ?")
+  public void cronJobScheduler() {
+
+  }
+
+
 }
