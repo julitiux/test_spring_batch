de0a80a0918c9ab4d96fb553945e9ce627c84d53
Julio Ramirez <rrodriguez.julio@gmail.com>
2024-09-04 13:38:35 -0600

    implement method cronJobScheduler

--- src/main/java/com/test_spring_batch/service/Scheduler.java
+++ src/main/java/com/test_spring_batch/service/Scheduler.java
@@ -3,12 +3,18 @@ package com.test_spring_batch.service;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 @Component
 public class Scheduler {
 
   @Scheduled(cron = "0 * 19 * * ?")
   public void cronJobScheduler() {
-
+    SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
+    Date now = new Date();
+    String stringDate = sdf.format(now);
+    System.out.println("Java cron job expressions %s%n".formatted(stringDate));
   }
 
 
