bfea2c2767eaeb271b55d8ec4dcef6f71e946120
Julio Ramirez <rrodriguez.julio@gmail.com>
2024-09-06 00:44:08 -0600

    add method dataSource and return a DataSource xD

--- src/main/java/com/test_spring_batch/config/ApplicationConfig.java
+++ src/main/java/com/test_spring_batch/config/ApplicationConfig.java
@@ -1,7 +1,16 @@
 package com.test_spring_batch.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class ApplicationConfig {
+
+  public DataSource dataSource() {
+    DriverManagerDataSource dataSource = new DriverManagerDataSource();
+    return dataSource;
+  }
+
 }
