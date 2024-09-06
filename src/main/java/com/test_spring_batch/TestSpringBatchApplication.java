bd53e6dede923b70eb15349275a61049360d6b68
Julio Ramirez <rrodriguez.julio@gmail.com>
2024-09-05 19:33:24 -0600

    delete tag EnableScheduling

--- src/main/java/com/test_spring_batch/TestSpringBatchApplication.java
+++ src/main/java/com/test_spring_batch/TestSpringBatchApplication.java
@@ -2,14 +2,12 @@ package com.test_spring_batch;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@EnableScheduling
 public class TestSpringBatchApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TestSpringBatchApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(TestSpringBatchApplication.class, args);
+  }
 
 }
