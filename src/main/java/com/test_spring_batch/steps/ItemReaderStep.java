0201f5089aa4553e419cdd05d7928139d1c649a5
Julio Ramirez <rrodriguez.julio@gmail.com>
2024-09-06 10:54:19 -0600

    inject the bean ResourceLoader

--- src/main/java/com/test_spring_batch/steps/ItemReaderStep.java
+++ src/main/java/com/test_spring_batch/steps/ItemReaderStep.java
@@ -1,13 +1,22 @@
 package com.test_spring_batch.steps;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ResourceLoader;
 
+@Slf4j
 public class ItemReaderStep implements Tasklet {
+
+  @Autowired
+  private ResourceLoader resourceLoader;
+
   @Override
   public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
+
     return null;
   }
 }
