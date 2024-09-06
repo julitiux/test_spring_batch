3c6e315d2db10275f91004ce8dddbf1b2d719ade
Julio Ramirez <rrodriguez.julio@gmail.com>
2024-09-06 10:40:05 -0600

    add implement an empty method execute into ItemReaderStep

--- src/main/java/com/test_spring_batch/steps/ItemReaderStep.java
+++ src/main/java/com/test_spring_batch/steps/ItemReaderStep.java
@@ -1,4 +1,13 @@
 package com.test_spring_batch.steps;
 
-public class ItemReaderStep {
+import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
+
+public class ItemReaderStep implements Tasklet {
+  @Override
+  public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
+    return null;
+  }
 }
