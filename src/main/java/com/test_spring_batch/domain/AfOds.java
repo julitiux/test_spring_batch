e78b913d76c5c053da21dfb6e97d2a580dc8fb2d
Julio Ramirez <rrodriguez.julio@gmail.com>
2024-09-06 00:25:46 -0600

    add properties into domain AfOds

--- src/main/java/com/test_spring_batch/domain/AfOds.java
+++ src/main/java/com/test_spring_batch/domain/AfOds.java
@@ -1,5 +1,12 @@
 package com.test_spring_batch.domain;
 
+import java.sql.Timestamp;
+
 public class AfOds {
+
+  private String codEnt;
+  private String IdListNegra;
+  private String codListNegra;
+  private Timestamp fchTimestampUmo;
 
 }
