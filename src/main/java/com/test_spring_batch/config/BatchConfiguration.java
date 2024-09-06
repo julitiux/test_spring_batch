ece674db9a54e9538f49455efa1dc58987ad83b2
Julio Ramirez <rrodriguez.julio@gmail.com>
2024-09-06 10:53:50 -0600

    add ItemReaderStep like a bean

--- src/main/java/com/test_spring_batch/config/BatchConfiguration.java
+++ src/main/java/com/test_spring_batch/config/BatchConfiguration.java
@@ -1,7 +1,15 @@
 package com.test_spring_batch.config;
 
+import com.test_spring_batch.steps.ItemReaderStep;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class BatchConfiguration {
+
+  @Bean
+  public ItemReaderStep itemReaderStep(){
+    return new ItemReaderStep();
+  }
+
 }
