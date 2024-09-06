56a5edd8d497990ef96368728e8f85d9d5de1761
Julio Ramirez <rrodriguez.julio@gmail.com>
2024-09-06 00:45:12 -0600

    add method dataSource into Spring context

--- src/main/java/com/test_spring_batch/config/ApplicationConfig.java
+++ src/main/java/com/test_spring_batch/config/ApplicationConfig.java
@@ -1,5 +1,6 @@
 package com.test_spring_batch.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
@@ -8,6 +9,7 @@ import javax.sql.DataSource;
 @Configuration
 public class ApplicationConfig {
 
+  @Bean
   public DataSource dataSource() {
     DriverManagerDataSource dataSource = new DriverManagerDataSource();
     return dataSource;
