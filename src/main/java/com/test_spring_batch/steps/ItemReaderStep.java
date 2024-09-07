fb8203bd880ea766f0dcc9c56be23fd7d31da7a0
Julio Ramirez <rrodriguez.julio@gmail.com>
2024-09-07 01:00:59 -0600

    add Reader. CSVParser and CSVReader

--- src/main/java/com/test_spring_batch/steps/ItemReaderStep.java
+++ src/main/java/com/test_spring_batch/steps/ItemReaderStep.java
@@ -1,5 +1,9 @@
 package com.test_spring_batch.steps;
 
+import com.opencsv.CSVParser;
+import com.opencsv.CSVParserBuilder;
+import com.opencsv.CSVReader;
+import com.opencsv.CSVReaderBuilder;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.scope.context.ChunkContext;
@@ -7,6 +11,9 @@ import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ResourceLoader;
+
+import java.io.FileReader;
+import java.io.Reader;
 
 @Slf4j
 public class ItemReaderStep implements Tasklet {
@@ -16,6 +23,16 @@ public class ItemReaderStep implements Tasklet {
 
   @Override
   public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
+
+    Reader reader = new FileReader(resourceLoader.getResource("classpath:emailBlackList.csv").getFile());
+
+    CSVParser parser = new CSVParserBuilder()
+      .withSeparator('|')
+      .build();
+
+    CSVReader csvReader = new CSVReaderBuilder(reader)
+      .withCSVParser(parser)
+      .build();
 
     return null;
   }
