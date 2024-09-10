e219d1a69d4a192b7efb1252ceb8cd3aa38cb1ff
Julio Ramirez <rrodriguez.julio@gmail.com>
2024-09-10 00:01:48 -0600

    get the list of AfOds, this information it was inside of spring bath context

--- src/main/java/com/test_spring_batch/steps/ItemProcessorStep.java
+++ src/main/java/com/test_spring_batch/steps/ItemProcessorStep.java
@@ -1,13 +1,24 @@
 package com.test_spring_batch.steps;
 
+import com.test_spring_batch.domain.AfOds;
 import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
 
+import java.util.List;
+
 public class ItemProcessorStep implements Tasklet {
   @Override
   public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
+
+    List<AfOds> afOdsList = (List<AfOds>) chunkContext
+      .getStepContext()
+      .getStepExecution()
+      .getJobExecution()
+      .getExecutionContext()
+      .get("afOdsList");
+
     return null;
   }
 }
