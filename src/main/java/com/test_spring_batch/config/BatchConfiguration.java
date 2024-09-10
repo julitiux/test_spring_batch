7eef7c6284dc5f9fb4509eb7794dd4197301323c
Julio Ramirez <rrodriguez.julio@gmail.com>
2024-09-10 00:37:55 -0600

    add methog itemProcessorStep like bean

--- src/main/java/com/test_spring_batch/config/BatchConfiguration.java
+++ src/main/java/com/test_spring_batch/config/BatchConfiguration.java
@@ -1,5 +1,6 @@
 package com.test_spring_batch.config;
 
+import com.test_spring_batch.steps.ItemProcessorStep;
 import com.test_spring_batch.steps.ItemReaderStep;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,8 +9,13 @@ import org.springframework.context.annotation.Configuration;
 public class BatchConfiguration {
 
   @Bean
-  public ItemReaderStep itemReaderStep(){
+  public ItemReaderStep itemReaderStep() {
     return new ItemReaderStep();
+  }
+
+  @Bean
+  public ItemProcessorStep itemProcessorStep() {
+    return new ItemProcessorStep();
   }
 
 }
