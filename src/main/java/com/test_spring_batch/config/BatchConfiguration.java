260a779bb2c400680cf3afa082ad2d6bb32d69ce
Julio Ramirez <rrodriguez.julio@gmail.com>
2024-09-10 01:02:26 -0600

    with EnableBatchProcessing we enable the Spring Batch configuration

--- src/main/java/com/test_spring_batch/config/BatchConfiguration.java
+++ src/main/java/com/test_spring_batch/config/BatchConfiguration.java
@@ -3,10 +3,12 @@ package com.test_spring_batch.config;
 import com.test_spring_batch.steps.ItemProcessorStep;
 import com.test_spring_batch.steps.ItemReaderStep;
 import com.test_spring_batch.steps.ItemWritterStep;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@EnableBatchProcessing
 public class BatchConfiguration {
 
   @Bean
