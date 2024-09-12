ef98cc6c8046a17f24c07e5374f5913492848b03
Julio Ramirez <rrodriguez.julio@gmail.com>
2024-09-12 14:05:13 -0600

    script to create table

---
+++ src/main/resources/db.migration/V1__init.sql
@@ -0,0 +1,10 @@
+CREATE TABLE afods
+(
+    id                  uuid,
+    cod_ent             VARCHAR,
+    id_list_negra       VARCHAR,
+    cod_list_negra      VARCHAR,
+    fch_timestamp_umo   TIMESTAMP,
+
+    PRIMARY KEY (id)
+);
