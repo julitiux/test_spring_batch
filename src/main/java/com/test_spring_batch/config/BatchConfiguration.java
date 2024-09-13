48ac375e8c37be90eb16c0952b60e61f7ab462fb
Julio Ramirez <rrodriguez.julio@gmail.com>
2024-09-12 23:05:37 -0600

    implement method itemReader

--- src/main/java/com/test_spring_batch/config/BatchConfiguration.java
+++ src/main/java/com/test_spring_batch/config/BatchConfiguration.java
@@ -1,10 +1,24 @@
 package com.test_spring_batch.config;
 
+import com.test_spring_batch.domain.AfOds;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.item.file.FlatFileItemReader;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.FileSystemResource;
 
 @Configuration
 @EnableBatchProcessing
 public class BatchConfiguration {
+
+  @Bean
+  public FlatFileItemReader<AfOds> itemReader() {
+    FlatFileItemReader<AfOds> itemReader = new FlatFileItemReader<>();
+    itemReader.setResource(new FileSystemResource("src/main/resources/emailBlackList.csv"));
+    itemReader.setName("csvReader");
+    itemReader.setLinesToSkip(1);
+    itemReader.setLineMapper(lineMapper());
+    return itemReader;
+  }
 
 }
