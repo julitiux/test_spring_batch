b74e54216b11031ceb5b60cf982a03d603daf016
Julio Ramirez <rrodriguez.julio@gmail.com>
2024-09-12 23:16:36 -0600

    implements ItemProcessor, his output and input is a AfOds that is process return like an AfOds

--- src/main/java/com/test_spring_batch/config/AfOdsProcessor.java
+++ src/main/java/com/test_spring_batch/config/AfOdsProcessor.java
@@ -1,4 +1,13 @@
 package com.test_spring_batch.config;
 
-public class AfOdsProcessor {
+import com.test_spring_batch.domain.AfOds;
+import org.springframework.batch.item.ItemProcessor;
+
+public class AfOdsProcessor implements ItemProcessor<AfOds, AfOds> {
+
+  @Override
+  public AfOds process(AfOds item) throws Exception {
+    return null;
+  }
+
 }
