03beddb32c7896f6f5a38e86d415e5e5100deac8
Julio Ramirez <rrodriguez.julio@gmail.com>
2024-09-12 23:18:06 -0600

    rename the param item -> afOds and return the same object

--- src/main/java/com/test_spring_batch/config/AfOdsProcessor.java
+++ src/main/java/com/test_spring_batch/config/AfOdsProcessor.java
@@ -6,8 +6,8 @@ import org.springframework.batch.item.ItemProcessor;
 public class AfOdsProcessor implements ItemProcessor<AfOds, AfOds> {
 
   @Override
-  public AfOds process(AfOds item) throws Exception {
-    return null;
+  public AfOds process(AfOds afOds) throws Exception {
+    return afOds;
   }
 
 }
