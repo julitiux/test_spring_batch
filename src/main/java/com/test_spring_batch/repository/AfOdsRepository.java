661fd1f7a3884dea9aff2ccd4c9beec45dafb774
Julio Ramirez <rrodriguez.julio@gmail.com>
2024-09-14 01:29:20 -0600

    delete steretype like Repository

--- src/main/java/com/test_spring_batch/repository/AfOdsRepository.java
+++ src/main/java/com/test_spring_batch/repository/AfOdsRepository.java
@@ -2,10 +2,8 @@ package com.test_spring_batch.repository;
 
 import com.test_spring_batch.domain.AfOds;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
-@Repository
 public interface AfOdsRepository extends JpaRepository<AfOds, UUID> {
 }
