bd1b70beb2c0828b8f36cde4f2fd40aeee69948a
Julio Ramirez <rrodriguez.julio@gmail.com>
2024-09-17 13:19:49 -0600

    implement method fixedRateScheduler

--- src/main/java/com/test_spring_batch/service/Scheduler.java
+++ src/main/java/com/test_spring_batch/service/Scheduler.java
@@ -1,4 +1,16 @@
 package com.test_spring_batch.service;
 
+import org.springframework.scheduling.annotation.Scheduled;
+
+import java.util.Date;
+
 public class Scheduler {
+
+  @Scheduled(fixedRate = 1000)
+  public void fixedRateScheduler() {
+
+    System.out.println(" {} " + new Date());
+
+  }
+
 }
