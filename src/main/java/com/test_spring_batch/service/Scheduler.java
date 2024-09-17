b9c1ed99f6fb29da54fb3f12d1947f2c03de631f
Julio Ramirez <rrodriguez.julio@gmail.com>
2024-09-17 13:24:43 -0600

    implement method fixedRateScheduler execiting every second

--- src/main/java/com/test_spring_batch/service/Scheduler.java
+++ src/main/java/com/test_spring_batch/service/Scheduler.java
@@ -1,16 +1,19 @@
 package com.test_spring_batch.service;
 
 import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
 
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class Scheduler {
 
   @Scheduled(fixedRate = 1000)
   public void fixedRateScheduler() {
-
-    System.out.println(" {} " + new Date());
-
+    SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
+    Date now = new Date();
+    String stringDate = sdf.format(now);
+    System.out.println("Fixed Rate Scheduler %s%n".formatted(stringDate));
   }
 
 }
