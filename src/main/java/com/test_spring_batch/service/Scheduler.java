5d1260e4b12cce48fabe3b7f216297f24bd8f1a4
Julio Ramirez <rrodriguez.julio@gmail.com>
2024-09-17 13:35:01 -0600

    modify the fixedRate to 10 seconds. inject the beans JobLauncher amd Job

--- src/main/java/com/test_spring_batch/service/Scheduler.java
+++ src/main/java/com/test_spring_batch/service/Scheduler.java
@@ -1,5 +1,14 @@
 package com.test_spring_batch.service;
 
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.JobParametersInvalidException;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
+import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
+import org.springframework.batch.core.repository.JobRestartException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -9,12 +18,34 @@ import java.util.Date;
 @Component
 public class Scheduler {
 
-  @Scheduled(fixedRate = 1000)
+  @Autowired
+  private JobLauncher jobLauncher;
+
+  @Autowired
+  private Job job;
+
+  @Scheduled(fixedRate = 10000)
   public void fixedRateScheduler() {
     SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
     Date now = new Date();
     String stringDate = sdf.format(now);
-    System.out.println("Fixed Rate Scheduler %s%n".formatted(stringDate));
+    System.out.println("Starting %s%n".formatted(stringDate));
+
+    JobParameters jobParameters = new JobParametersBuilder()
+      .addLong("startAt", System.currentTimeMillis())
+      .toJobParameters();
+
+    try {
+      jobLauncher.run(job, jobParameters);
+    } catch (JobExecutionAlreadyRunningException | JobRestartException | JobInstanceAlreadyCompleteException |
+             JobParametersInvalidException e) {
+      throw new RuntimeException(e);
+    }
+
+    now = new Date();
+    stringDate = sdf.format(now);
+    System.out.println("Ending %s%n".formatted(stringDate));
+
   }
 
 }
