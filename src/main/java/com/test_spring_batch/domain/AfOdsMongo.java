da8ef58e254be1cea160669fbb7eb75f7822191b
Julio Ramirez <rrodriguez.julio@gmail.com>
2024-09-18 13:42:19 -0600

    add properties mongo into object AfOdsMongo

--- src/main/java/com/test_spring_batch/domain/AfOdsMongo.java
+++ src/main/java/com/test_spring_batch/domain/AfOdsMongo.java
@@ -1,4 +1,20 @@
 package com.test_spring_batch.domain;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.UUID;
+
+@Document("AfOdsMongo")
 public class AfOdsMongo {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private UUID id;
+  private String codeEntity;
+  private String idListBlack;
+  private String codeListBlack;
+
 }
