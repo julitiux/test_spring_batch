e3846aea6c83aafdda1c778e9700f3562937dbbd
Julio Ramirez <rrodriguez.julio@gmail.com>
2024-09-19 01:18:55 -0600

    implement method writer and get values from yml

--- src/main/java/com/test_spring_batch/config/MongoConfiguration.java
+++ src/main/java/com/test_spring_batch/config/MongoConfiguration.java
@@ -1,17 +1,33 @@
 package com.test_spring_batch.config;
 
+import com.mongodb.client.MongoClients;
 import com.test_spring_batch.domain.AfOdsMongo;
+import org.springframework.batch.item.data.MongoItemWriter;
 import org.springframework.batch.item.file.FlatFileItemReader;
 import org.springframework.batch.item.file.LineMapper;
 import org.springframework.batch.item.file.mapping.BeanWrapperFieldSetMapper;
 import org.springframework.batch.item.file.mapping.DefaultLineMapper;
 import org.springframework.batch.item.file.transform.DelimitedLineTokenizer;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.FileSystemResource;
+import org.springframework.data.mongodb.core.MongoTemplate;
 
 @Configuration
 public class MongoConfiguration {
+
+  @Value("${spring.data.mongodb.uri}")
+  private String mongoUri;
+
+  @Value("${spring.data.mongodb.database}")
+  private String mongoDatabase;
+
+  @Bean
+  public MongoTemplate mongoTemplate() {
+    // Crear una conexión con MongoDB usando el URI y pasarla a MongoTemplate
+    return new MongoTemplate(MongoClients.create(mongoUri), mongoDatabase);
+  }
 
   @Bean
   public FlatFileItemReader<AfOdsMongo> itemReader() {
@@ -24,8 +40,16 @@ public class MongoConfiguration {
   }
 
   @Bean
-  public AfOdsMongoProcessor processor(){
+  public AfOdsMongoProcessor processor() {
     return new AfOdsMongoProcessor();
+  }
+
+  @Bean
+  public MongoItemWriter<AfOdsMongo> writer() {
+    MongoItemWriter<AfOdsMongo> writer = new MongoItemWriter<>();
+    writer.setTemplate(mongoTemplate());
+    writer.setCollection("AfOdsMongo");
+    return writer;
   }
 
 
