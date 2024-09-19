cc35bb235fc228ddb90a75ba66ce924060d49a82
Julio Ramirez <rrodriguez.julio@gmail.com>
2024-09-19 00:45:06 -0600

    change import into AfOdsMongo

--- src/main/java/com/test_spring_batch/domain/AfOdsMongo.java
+++ src/main/java/com/test_spring_batch/domain/AfOdsMongo.java
@@ -1,18 +1,16 @@
 package com.test_spring_batch.domain;
 
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+
+import lombok.Data;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-import java.util.UUID;
-
 @Document("AfOdsMongo")
+@Data
 public class AfOdsMongo {
 
   @Id
-  @GeneratedValue(strategy = GenerationType.AUTO)
-  private UUID id;
+  private String id;
   private String codeEntity;
   private String idListBlack;
   private String codeListBlack;
