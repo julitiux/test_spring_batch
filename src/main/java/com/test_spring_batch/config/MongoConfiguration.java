385bfdcec540f3819386d5ec85a011213666c0fa
Julio Ramirez <rrodriguez.julio@gmail.com>
2024-09-19 00:56:05 -0600

    implement method itemReader and return FlatFileItemReader<AfOdsMongo>

--- src/main/java/com/test_spring_batch/config/MongoConfiguration.java
+++ src/main/java/com/test_spring_batch/config/MongoConfiguration.java
@@ -1,7 +1,22 @@
 package com.test_spring_batch.config;
 
+import com.test_spring_batch.domain.AfOdsMongo;
+import org.springframework.batch.item.file.FlatFileItemReader;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.FileSystemResource;
 
 @Configuration
 public class MongoConfiguration {
+
+  @Bean
+  public FlatFileItemReader<AfOdsMongo> itemReader() {
+    FlatFileItemReader<AfOdsMongo> itemReader = new FlatFileItemReader<>();
+    itemReader.setResource(new FileSystemResource("src/main/resources/emailBlackList.csv"));
+    itemReader.setName("csvReaderForMongo");
+    itemReader.setLinesToSkip(1);
+    itemReader.setLineMapper(lineMapper());
+    return itemReader;
+  }
+
 }
