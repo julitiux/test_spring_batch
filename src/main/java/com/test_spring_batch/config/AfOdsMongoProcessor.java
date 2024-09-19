304cf2cf5b063e3879ed1b41d43d4db9bee0d637
Julio Ramirez <rrodriguez.julio@gmail.com>
2024-09-19 00:59:24 -0600

    add interface ItemProcessor, recive an AfOdsMongo and return an AfOdsMongo using the method process

--- src/main/java/com/test_spring_batch/config/AfOdsMongoProcessor.java
+++ src/main/java/com/test_spring_batch/config/AfOdsMongoProcessor.java
@@ -1,4 +1,13 @@
 package com.test_spring_batch.config;
 
-public class AfOdsMongoProcessor {
+import com.test_spring_batch.domain.AfOdsMongo;
+import org.springframework.batch.item.ItemProcessor;
+
+public class AfOdsMongoProcessor implements ItemProcessor<AfOdsMongo, AfOdsMongo> {
+
+  @Override
+  public AfOdsMongo process(AfOdsMongo afOdsMongo) throws Exception {
+    return afOdsMongo;
+  }
+
 }
