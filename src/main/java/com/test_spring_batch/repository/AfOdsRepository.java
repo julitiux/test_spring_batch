86c7daccf1d77946fee7b07afefcef7d11be027f
Julio Ramirez <rrodriguez.julio@gmail.com>
2024-09-23 00:55:19 -0600

    delete all objects that works with database PostgreSQL

--- src/main/java/com/test_spring_batch/repository/AfOdsRepository.java
+++
@@ -1,9 +0,0 @@
-package com.test_spring_batch.repository;
-
-import com.test_spring_batch.domain.AfOds;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.UUID;
-
-public interface AfOdsRepository extends JpaRepository<AfOds, UUID> {
-}
