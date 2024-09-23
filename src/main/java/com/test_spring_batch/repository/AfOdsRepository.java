61a7b2ccf390d146993e879475b044a666c59ed7
Julio Ramirez <rrodriguez.julio@gmail.com>
2024-09-23 01:01:03 -0600

    rename AfOdsMongoRepository -> AfOdsRepository

--- src/main/java/com/test_spring_batch/repository/AfOdsRepository.java
+++ src/main/java/com/test_spring_batch/repository/AfOdsRepository.java
@@ -3,5 +3,5 @@ package com.test_spring_batch.repository;
 import com.test_spring_batch.domain.AfOdsMongo;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-public interface AfOdsMongoRepository extends MongoRepository<AfOdsMongo, String> {
+public interface AfOdsRepository extends MongoRepository<AfOdsMongo, String> {
 }
