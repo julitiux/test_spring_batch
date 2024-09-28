fb2ab3e031c0810a0df46eb2bb7032eb5e5d7b81
Julio Ramirez <rrodriguez.julio@gmail.com>
2024-09-27 23:12:12 -0600

    add implementation into execute method

--- src/main/java/com/test_spring_batch/config/SftpDownloadTasklet.java
+++ src/main/java/com/test_spring_batch/config/SftpDownloadTasklet.java
@@ -4,14 +4,25 @@ import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.integration.sftp.inbound.SftpInboundFileSynchronizingMessageSource;
+import org.springframework.messaging.Message;
 import org.springframework.stereotype.Component;
 
 @Component
 public class SftpDownloadTasklet implements Tasklet {
 
+  @Autowired
+  SftpInboundFileSynchronizingMessageSource sftpMessageSource;
+
   @Override
   public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
-    return null;
+    // Aquí puedes ejecutar la lógica para sincronizar archivos desde el SFTP
+    Message<?> message;
+    while ((message = sftpMessageSource.receive()) != null) {
+      System.out.println("Archivo descargado: " + message.getPayload());
+    }
+    return RepeatStatus.FINISHED;
   }
 
 }
