f54f20ad458ca665dab31377227eebaa632e1a1c
Julio Ramirez <rrodriguez.julio@gmail.com>
2024-09-27 23:09:23 -0600

    implements Tasklet into SftpDownloadTasklet

--- src/main/java/com/test_spring_batch/config/SftpDownloadTasklet.java
+++ src/main/java/com/test_spring_batch/config/SftpDownloadTasklet.java
@@ -1,4 +1,15 @@
 package com.test_spring_batch.config;
 
-public class SftpDownloadTasklet {
+import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
+
+public class SftpDownloadTasklet implements Tasklet {
+
+  @Override
+  public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
+    return null;
+  }
+
 }
