2da10adb4128f7616aedf161869f77617f79a6ca
Julio Ramirez <rrodriguez.julio@gmail.com>
2024-09-27 20:25:05 -0600

    implement method sftpInboundFileSynchronizer

--- src/main/java/com/test_spring_batch/config/SftpConfig.java
+++ src/main/java/com/test_spring_batch/config/SftpConfig.java
@@ -2,6 +2,8 @@ package com.test_spring_batch.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.integration.sftp.filters.SftpSimplePatternFileListFilter;
+import org.springframework.integration.sftp.inbound.SftpInboundFileSynchronizer;
 import org.springframework.integration.sftp.session.DefaultSftpSessionFactory;
 
 @Configuration
@@ -16,6 +18,15 @@ public class SftpConfig {
     factory.setPassword("password");
     factory.setAllowUnknownKeys(true);
     return factory;
+  }
+
+  @Bean
+  public SftpInboundFileSynchronizer sftpInboundFileSynchronizer() {
+    SftpInboundFileSynchronizer synchronizer = new SftpInboundFileSynchronizer(sftpSessionFactory());
+    synchronizer.setDeleteRemoteFiles(false);  // Si no quieres eliminar los archivos después de descargarlos
+    synchronizer.setRemoteDirectory("/remote/path"); // Directorio en el servidor SFTP
+    synchronizer.setFilter(new SftpSimplePatternFileListFilter("*.txt")); // Filtra archivos según su patrón
+    return synchronizer;
   }
 
 }
