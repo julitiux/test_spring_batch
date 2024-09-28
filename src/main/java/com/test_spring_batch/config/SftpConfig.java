9c8484dca161694431df8b57123dd04ce6c2407c
Julio Ramirez <rrodriguez.julio@gmail.com>
2024-09-27 20:21:11 -0600

    implement method sftpSessionFactory

--- src/main/java/com/test_spring_batch/config/SftpConfig.java
+++ src/main/java/com/test_spring_batch/config/SftpConfig.java
@@ -1,7 +1,21 @@
 package com.test_spring_batch.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.integration.sftp.session.DefaultSftpSessionFactory;
 
 @Configuration
 public class SftpConfig {
+
+  @Bean
+  public DefaultSftpSessionFactory sftpSessionFactory() {
+    DefaultSftpSessionFactory factory = new DefaultSftpSessionFactory();
+    factory.setHost("localhost");         // Cambia el host al de tu servidor SFTP
+    factory.setPort(2222);                // Cambia el puerto si es necesario
+    factory.setUser("user");
+    factory.setPassword("password");
+    factory.setAllowUnknownKeys(true);
+    return factory;
+  }
+
 }
